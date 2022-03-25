0b8443794162c4c821a428cb68247c69b33b0f19
Franco Basilio <basiliofranco1999@gmail.com>
2022-03-25 11:30:24 -0300

    Agregamos plantillas

---
+++ index/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import URLPattern, path
+from .views import index, inicio
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('inicio/', inicio, name='inicio'),
+]
